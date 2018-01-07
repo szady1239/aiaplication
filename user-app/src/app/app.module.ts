4c650db88d3e8508ac8a44b6edaeb8c57ee8274e
PKOWALCZYK <pawel.kowalczyk@execon.pl>
2018-01-07 21:20:36 +0100

    http import fix

--- user-app/src/app/app.module.ts
+++ user-app/src/app/app.module.ts
@@ -4,19 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { CarListComponent } from './carparts/car-list/car-list.component';
-import { PartGroupListComponent } from './carparts/part-group-list/part-group-list.component';
-import { PartsComponent } from './carparts/parts/parts.component';
-import { OrderCartComponent } from './carparts/order-cart/order-cart.component';
 import { CarpartsModule} from './carparts/carparts.module';
-
+import {HttpModule} from "@angular/http";
 @NgModule({
   declarations: [
-    AppComponent,
-    CarListComponent,
-    PartGroupListComponent,
-    PartsComponent,
-    OrderCartComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
